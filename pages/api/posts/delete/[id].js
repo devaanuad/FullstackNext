28495690789488e7b4fb02a6b1d53016b7b83b9f
unknown <you@example.com>
2022-04-20 10:17:46 +0700

    add middlewares

--- pages/api/posts/delete/[id].js
+++ pages/api/posts/delete/[id].js
@@ -1,8 +1,12 @@
 import db from '../../../../libs/db';
+import authorization from '../../../../middlewares/authorization';
 
 export default async function handler(req, res) {
 	if (req.method !== 'DELETE') return req.status(405).end();
 
+	//manggil eksekusi auth dulu ke middlewares
+	const auth = await authorization(req, res);
+	
 	const { id } = req.query ;
 
 	const deleteRow = await db('posts')
--- pages/api/posts/update/[id].js
+++ pages/api/posts/update/[id].js
@@ -1,7 +1,11 @@
 import db from '../../../../libs/db';
+import authorization from '../../../../middlewares/authorization';
 
 export default async function handler(req, res) {
 	if (req.method !== 'PUT') return req.status(405).end();
+
+	//manggil eksekusi auth dulu ke middlewares
+	const auth = await authorization(req, res);
 
 	const { id } = req.query ;
 	const { title, content }	= req.body;
